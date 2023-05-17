d6bc5a3be4ce3b46123b2866e058c3b5b48e3450
Rodrigo Falcão <125101340+rodrigo-falcao@users.noreply.github.com>
2023-05-16 23:34:56 -0300

    primeira atualização

---
+++ src/components/PainelCentral/PainelCentral.js
@@ -0,0 +1,15 @@
+import "./PainelCentral.css"
+
+const PainelCentral = () => {
+    return (
+        <section>
+            <h3>Muito Obrigado Joao!!</h3>
+            <p>Agradecemos a sua compra. Em breve você receberá um 
+                email com a confirmação e acompanhamento do seu pedido
+                </p>
+            <button>Voltar as Compras!</button>
+        </section>
+    )
+};
+
+export default PainelCentral
